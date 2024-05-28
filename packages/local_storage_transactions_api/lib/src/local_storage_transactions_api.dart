0f32444d49311c44af114a698ce460c2ffc029b3
terrence <terrencedbeckham@gmail.com>
2024-05-28 10:29:47 +0300

    Initial Set up of local_storage_transactions_api.dart

--- packages/local_storage_transactions_api/lib/src/local_storage_transactions_api.dart
+++ packages/local_storage_transactions_api/lib/src/local_storage_transactions_api.dart
@@ -1,7 +1,59 @@
+import 'package:isar/isar.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:transactions_api/transactions_api.dart';
+
 /// {@template local_storage_transactions_api}
 /// A Flutter implementation of the TransactionsApi that uses local storage.
 /// {@endtemplate}
-class LocalStorageTransactionsApi {
+class LocalStorageTransactionsApi extends TransactionsApi {
   /// {@macro local_storage_transactions_api}
-  const LocalStorageTransactionsApi();
+  LocalStorageTransactionsApi({required Isar isarDb}) : _isarDb = isarDb {
+    init();
+  }
+
+  final Isar _isarDb;
+  late final _transactionStreamController =
+      BehaviorSubject<List<Transaction>>.seeded(const []);
+  late final _transactionCategoryStreamController =
+      BehaviorSubject<List<TransactionCategory>>.seeded(const []);
+
+  ///Initialize both stream controllers.
+  Future<void> init() async {
+    final transactions = _isarDb.transactions as List<Transaction>;
+    _transactionStreamController.add(transactions);
+
+    final transactionCategories =
+        _isarDb.transactionCategorys as List<TransactionCategory>;
+    _transactionCategoryStreamController.add(transactionCategories);
+  }
+
+  @override
+  Future<void> deleteTransaction(String id) {
+    // TODO: implement deleteTransaction
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<List<Transaction>> getTransactions() {
+    // TODO: implement getTransactions
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<List<PieChartDataObject>> getTransactionsByCategory() {
+    // TODO: implement getTransactionsByCategory
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> saveTransaction(Transaction transaction) {
+    // TODO: implement saveTransaction
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> close() {
+    _transactionStreamController.close();
+    return _transactionCategoryStreamController.close();
+  }
 }
--- packages/transactions_api/lib/src/transactions_api.dart
+++ packages/transactions_api/lib/src/transactions_api.dart
@@ -27,15 +27,8 @@ abstract class TransactionsApi {
   /// thrown.
   Future<void> deleteTransaction(String id);
 
-  /// Deletes all completed transactions.
-  ///
-  /// Returns the number of deleted todos.
-  Future<int> clearCompleted();
 
-  /// Sets the `isCompleted` state of all transactions to the given value.
-  ///
-  /// Returns the number of updated transactions.
-  Future<int> completeAll({required bool isCompleted});
+
 
   /// Closes the client and frees up any resources.
   Future<void> close();
