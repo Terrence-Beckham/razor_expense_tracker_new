62793214ec97ef6eb5f2d02fed514033cf287d2c
terrence <terrencedbeckham@gmail.com>
2024-05-28 08:52:28 +0300

    Set up transactions_api.dart

--- packages/transactions_api/lib/src/transactions_api.dart
+++ packages/transactions_api/lib/src/transactions_api.dart
@@ -1,7 +1,41 @@
+import 'package:transactions_api/src/models/transaction.dart';
+
 /// {@template transactions_api}
 /// The interface and models for an API providing access to transactions.
 /// {@endtemplate}
-class TransactionsApi {
-  /// {@macro transactions_api}
+
+abstract class TransactionsApi {
+  /// {@macro todos_api}
   const TransactionsApi();
+
+  /// Provides a [Stream] of all todos.
+  Stream<List<Transaction>> getTransactions();
+
+  /// Saves a [transaction].
+  ///
+  /// If a [Transaction] with the same id already exists, it will be replaced.
+  Future<void> saveTransaction(Transaction transaction);
+
+  /// Deletes the `transaction` with the given id.
+  ///
+  /// If no `transaction` with the given id exists, a
+  /// [TransactionNotFoundException] error is
+  /// thrown.
+  Future<void> deleteTransaction(String id);
+
+  /// Deletes all completed transactions.
+  ///
+  /// Returns the number of deleted todos.
+  Future<int> clearCompleted();
+
+  /// Sets the `isCompleted` state of all transactions to the given value.
+  ///
+  /// Returns the number of updated transactions.
+  Future<int> completeAll({required bool isCompleted});
+
+  /// Closes the client and frees up any resources.
+  Future<void> close();
 }
+
+/// Error thrown when a [Transaction] with a given id is not found.
+class TransactionNotFoundException implements Exception {}
--- packages/transactions_api/lib/transactions_api.dart
+++ packages/transactions_api/lib/transactions_api.dart
@@ -1,4 +1,5 @@
 /// The interface and models for an API providing access to transactions.
 library;
 
+export 'src/models/transaction_category.dart';
 export 'src/transactions_api.dart';
